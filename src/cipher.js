a8bfe02b6febcb81d72d0d1f81a80a91097e95f1
JoannaAnaiz <joanna.yucra@gmail.com>
2019-06-13 12:23:34 -0500

    agregando loggin

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,26 @@
 window.cipher = {
-  encode: (offset, string) => { hola
-    /* Acá va tu código que cifra*/
-  },
-  decode: (offset, string) => {
-    /* Acá va tu código que descifra*/
+  encode: (entrada1,offsetNumber1) => {
+    let codificar = "";
+    for(let i=0; i<entrada1.length; i++){ 
+      let ascii1 = entrada1.charCodeAt(i);
+      //if(ascii1>=32 && ascii1<=126){
+        codificar += String.fromCharCode((ascii1-32+offsetNumber1)%95+32);
+       // }
+      //  if(ascii1>=161 && ascii<=256){
+         codificar += String.fromCharCode((ascii1-161+offsetNumber1)%95+161);
+      //  }
+       }
+       return codificar;
+    },
+  decode: (entrada2,offsetNumber2) => {
+    let decodificar = "";
+    for (let i=0; i<entrada2.length; i++){ 
+      let ascii2 = entrada2.charCodeAt(i);
+      //if(ascii2>=32 && ascii2<=126){
+        decodificar += String.fromCharCode((ascii2-offsetNumber2+32)%95+32);
+        //}
+        
+        }
+        return decodificar;
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-/* Acá va tu código */
+const ingresar = document.getElementById('ingresar');
+const pagina1 = document.getElementById('pagina1');
+const pagina2 = document.getElementById('pagina2');
+
+const password2 = 'LABORATORIA';
+let nroIntentos = 0;
+
+const confirmar = () =>{  
+  if(password1.value === password2){
+    pagina1.classList.add('hide');
+    pagina2.classList.replace('hide','show');
+    console.log ('Lo Lograste')
+  }
+  else{
+      if(nroIntentos<2){
+        password1.value = ''
+        nroIntentos++;
+      }else{
+        pagina1.classList.add('hide');
+        pagina3.classList.replace('hide','show');
+        console.log('Intentalo de nuevo');
+      }
+     
+      }
+   }
+
+ingresar.addEventListener('click',confirmar);
+
