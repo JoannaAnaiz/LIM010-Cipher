37a03ff80e6bd81860a55464be3f3db0460997b1
JoannaAnaiz <joanna.yucra@gmail.com>
2019-06-17 13:04:01 -0500

    corrigiendo decodificado

--- src/cipher.js
+++ src/cipher.js
@@ -15,6 +15,9 @@ window.cipher = {
       }else if(codAscii >= 32 && codAscii <=47){
         let code = (codAscii - 32 + desplazamiento)%16 + 32;
         codificar += String.fromCharCode(code);
+      }else if(codAscii >= 58 && codAscii <=64){
+        let code = (codAscii - 58 + desplazamiento)%7 + 58;
+        codificar += String.fromCharCode(code);
          
       }
       
@@ -26,16 +29,19 @@ window.cipher = {
     for(let i = 0; i< entrada1.length; i++){
       let codAscii = entrada1.charCodeAt(i);
       if(codAscii >= 65 && codAscii <= 90){
-        let code = (codAscii + 65 - desplazamiento)%26 + 65 ;
+        let code = 90 - (90 - codAscii  + desplazamiento)%26 ;
         decodificar += String.fromCharCode(code);
       }else if(codAscii >= 97 && codAscii <= 122){
-        let code = (codAscii + 85 - desplazamiento)%26 + 97;
+        let code = 122 - (122 - codAscii + desplazamiento)%26;
         decodificar += String.fromCharCode(code);
       }else if(codAscii >= 48 && codAscii <= 57){
-        let code = (codAscii + 52 - desplazamiento)%10 + 48;
+        let code = 57 - (57 - codAscii + desplazamiento)%10;
         decodificar += String.fromCharCode(code);
       }else if(codAscii >= 32 && codAscii <=47){
-        let code = (codAscii + 32 - desplazamiento)%16 + 32;
+        let code = 47 - (47 - codAscii + desplazamiento)%16;
+        decodificar += String.fromCharCode(code);
+      }else if(codAscii >= 58 && codAscii <=64){
+        let code = 64 - (64 - codAscii + desplazamiento)%7;
         decodificar += String.fromCharCode(code);
       }
     }
